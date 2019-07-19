b20e138a716abc8d9003744e47507f6e621fe671
Nikita Merkel <namerkel@mts.ru>
2019-07-19 11:08:30 +0300

    Initial Commit

---
+++ MoviesListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesListApp
+//
+//  Created by Nikita Merkel on 19/07/2019.
+//  Copyright © 2019 Nikita Merkel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
